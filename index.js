9ed2de18a14b486d426521f840ce4e9535dfd2a4
Amur <Klyuev_Andrey@mail.ru>
2020-08-24 15:32:21 +0300

    init

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express')
 
 const app = express()
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 const path = require("path");
 const cors = require('cors')
 app.use(cors())
-app.use(express.static(path.join(__dirname, "client/build")));
+
 if (process.env.NODE_ENV === "production"){
     app.use(express.static(path.join(__dirname, "client/build")));
 }
