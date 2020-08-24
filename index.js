a4de4d0fdea2858aa4ee4e691d2e8fe692afb7e2
Amur <Klyuev_Andrey@mail.ru>
2020-08-24 16:21:37 +0300

    init

--- index.js
+++ index.js
@@ -23,10 +23,10 @@ const devConfig = {
 }
 
 const proConfig = {
-    connectString: process.env.DATABASE_URL
+    connectionString: process.env.DATABASE_URL
 }
 
-const pool = new Pool({connectString: process.env.NODE_ENV === "production" ? proConfig : devConfig});
+const pool = new Pool({connectionString: 'postgres://sypfjpqxhupflq:145894f615218008daccdc77094eefc50223ba2e5cb46916171c23673ce9c18c@ec2-34-192-122-0.compute-1.amazonaws.com:5432/deqirk1855e1hq'});
 
 app.use( express.json() );       // to support JSON-encoded bodies
 app.use(express.urlencoded({     // to support URL-encoded bodies
