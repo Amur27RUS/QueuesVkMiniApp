83d690d80afd13908c164f36dc142c088b29123e
Amur <Klyuev_Andrey@mail.ru>
2020-08-27 00:56:20 +0300

    init

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.30eb49928799eaacd63402b4d3aca26c.js"
+  "./precache-manifest.6b40ac1d1d5197f4e6148c19677c17ee.js"
 );
 
 self.addEventListener('message', (event) => {
