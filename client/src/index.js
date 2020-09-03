fc2892a32047717a66f99db35b9f6987917d2675
Amur <Klyuev_Andrey@mail.ru>
2020-09-03 13:10:34 +0300

    security configs

--- client/src/index.js
+++ client/src/index.js
@@ -8,6 +8,29 @@ import App from "./App";
 global.scheme = {
     scheme: undefined,
 }
+const qs = require('querystring');
+const crypto = require('crypto');
+
+const urlParams = qs.parse(URL_PARAMS);
+const ordered = {};
+Object.keys(urlParams).sort().forEach((key) => {
+    if (key.slice(0, 3) === 'vk_') {
+        ordered[key] = urlParams[key];
+    }
+});
+
+const stringParams = qs.stringify(ordered);
+const paramsHash = crypto
+    .createHmac('sha256', secretKey)
+    .update(stringParams)
+    .digest()
+    .toString('base64')
+    .replace(/\+/g, '-')
+    .replace(/\//g, '_')
+    .replace(/=$/, '');
+
+console.log('TEST')
+console.log(paramsHash === urlParams.sign);
 
 // Init VK  Mini App
 bridge.send("VKWebAppInit");
