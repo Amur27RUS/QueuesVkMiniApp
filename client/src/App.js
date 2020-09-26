be5d47e2875db6e36ce9f8aa50b34536cb64eaf8
pavel <pavels21@bk.ru>
2020-09-26 18:45:10 +0300

    add swipe

--- client/src/App.js
+++ client/src/App.js
@@ -459,8 +459,8 @@ const App = () =>{
 						title: 'Присоединиться',
 						mode: 'primary',
 						action: () => {
-							history.pop() // удаляем последний элемент в массиве.
-							setActivePanel( history[history.length - 1] ) // Изменяем массив с иторией и меняем активную панель.
+							// history.pop() // удаляем последний элемент в массиве.
+							// setActivePanel( history[history.length - 1] ) // Изменяем массив с иторией и меняем активную панель.
 							if(codeInput !== undefined) {
 								sendDataToServer(codeInput.toUpperCase());
 							}else{
