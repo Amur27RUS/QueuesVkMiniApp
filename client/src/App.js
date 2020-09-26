d7ae4b02a9c4a7ec777edc4b1e3a0dbfe753926b
pavel <pavels21@bk.ru>
2020-09-26 19:14:45 +0300

    add swipe

--- client/src/App.js
+++ client/src/App.js
@@ -267,14 +267,16 @@ const App = () =>{
 	}, []);
 
 	const goBack = () => {
-		if( history.length === 1 ) {  // Если в массиве одно значение:
-			bridge.send("VKWebAppClose", {"status": "success"}); // Отправляем bridge на закрытие сервиса.
-		} else if( history.length > 1 ) { // Если в массиве больше одного значения:
-			history.pop() // удаляем последний элемент в массиве.
-			setActivePanel( history[history.length - 1] ) // Изменяем массив с иторией и меняем активную панель.
+		if (popout === null) {
+			if (history.length === 1) {  // Если в массиве одно значение:
+				bridge.send("VKWebAppClose", {"status": "success"}); // Отправляем bridge на закрытие сервиса.
+			} else if (history.length > 1) { // Если в массиве больше одного значения:
+				history.pop() // удаляем последний элемент в массиве.
+				setActivePanel(history[history.length - 1]) // Изменяем массив с иторией и меняем активную панель.
+			}
 		}
+		else {setPopout(null)}
 	}
-
 	const go = e => {
 		setActivePanel(e.currentTarget.dataset.to);
 		setSnackbar(null); //При переходе
