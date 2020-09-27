11f75645d94e4b3701ec782e4554a0d6e16d121a
Amur <Klyuev_Andrey@mail.ru>
2020-09-27 11:01:43 +0300

    fixed some bugs...

--- client/src/App.js
+++ client/src/App.js
@@ -269,14 +269,16 @@ const App = () =>{
 
 	const goBack = () => {
 		setSnackbar(null);
-		if (popout === null) {
+		if (popout !== null) {
+			setPopout(null);
+		} else {
 			if (history.length === 1) {  // Если в массиве одно значение:
 				bridge.send("VKWebAppClose", {"status": "success"}); // Отправляем bridge на закрытие сервиса.
 			} else if (history.length > 1) { // Если в массиве больше одного значения:
 				history.pop() // удаляем последний элемент в массиве.
 				setActivePanel(history[history.length - 1]) // Изменяем массив с иторией и меняем активную панель.
 			}
-		} else {setPopout(null)}
+		}
 	}
 
 	const go = e => {
