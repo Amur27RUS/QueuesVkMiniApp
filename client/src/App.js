79960e0c7857d3aed9aae25ab93b9a998e211af7
pavel <pavels21@bk.ru>
2020-09-28 13:40:35 +0300

    add swipe

--- client/src/App.js
+++ client/src/App.js
@@ -271,13 +271,16 @@ const App = () =>{
 		setSnackbar(null);
 		if (popout !== null) {
 			setPopout(null);
-		} else {
-			if (history.length === 1) {  // Если в массиве одно значение:
+		} else { if (activeModal !== null) {
+			setActiveModal(null);
+		}
+			else {if (history.length === 1) {  // Если в массиве одно значение:
 				bridge.send("VKWebAppClose", {"status": "success"}); // Отправляем bridge на закрытие сервиса.
 			} else if (history.length > 1) { // Если в массиве больше одного значения:
 				history.pop() // удаляем последний элемент в массиве.
 				setActivePanel(history[history.length - 1]) // Изменяем массив с иторией и меняем активную панель.
 			}
+		}
 		}
 	}
 
@@ -509,10 +512,10 @@ const App = () =>{
 					title: 'На страницу с очередями',
 					mode: 'primary',
 					action: () => {
-						history.pop() // удаляем последний элемент в массиве.
-						setActivePanel( history[history.length - 1] ) // Изменяем массив с иторией и меняем активную панель.
-						window.history.pushState( {panel: "home"}, "home" ); // Создаём новую запись в истории браузера
-						history.push("home"); // Добавляем панель в историю
+						// history.pop() // удаляем последний элемент в массиве.
+						// setActivePanel( history[history.length - 1] ) // Изменяем массив с иторией и меняем активную панель.
+						// window.history.pushState( {panel: "home"}, "home" ); // Создаём новую запись в истории браузера
+						// history.push("home"); // Добавляем панель в историю
 						setActiveModal(null);
 						setActiveStory('main');
 						setActivePanel('home');
