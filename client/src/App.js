f12fa74509bd955831262c81114ed1a190f945ee
pavel <pavels21@bk.ru>
2020-10-01 17:26:49 +0300

    add swipe

--- client/src/App.js
+++ client/src/App.js
@@ -271,20 +271,22 @@ const App = () =>{
 	}, []);
 
 	const goBack = () => {
-		console.log(activePanel)
+		console.log('ActivePanel: ' + activePanel)
 		if (activePanel === 'home' || activePanel === 'CreateQueue' || activeStory === 'createQueue') {
 			bridge.send('VKWebAppDisableSwipeBack');
 		}
-		else {bridge.send('VKWebAppEnableSwipeBack');
-		setSnackbar(null);
-		setActiveModal(null);
-		setPopout(null);
-		if (history.length === 1) {  // Если в массиве одно значение:
+		else {
+			bridge.send('VKWebAppEnableSwipeBack');
+			setSnackbar(null);
+			setActiveModal(null);
+			setPopout(null);
+			if (history.length === 1) {  // Если в массиве одно значение:
 				// bridge.send("VKWebAppClose", {"status": "success"}); // Отправляем bridge на закрытие сервиса.
-		} else
-			if (history.length > 1) { // Если в массиве больше одного значения:
-				history.pop() // удаляем последний элемент в массиве.
-				setActivePanel(history[history.length - 1]) // Изменяем массив с иторией и меняем активную панель.
+			} else {
+				if (history.length > 1) { // Если в массиве больше одного значения:
+					history.pop() // удаляем последний элемент в массиве.
+					setActivePanel(history[history.length - 1]) // Изменяем массив с иторией и меняем активную панель.
+				}
 			}
 
 		}
