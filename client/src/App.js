cb1458e882ef8860d0c4423dd0224a667a9460d1
pavel <pavels21@bk.ru>
2020-10-04 17:34:50 +0300

    add swipe

--- client/src/App.js
+++ client/src/App.js
@@ -278,7 +278,7 @@ const App = () =>{
 			setActiveModal(null);
 			setPopout(null);
 			if (history.length === 1) {  // Если в массиве одно значение:
-				// bridge.send("VKWebAppClose", {"status": "success"}); // Отправляем bridge на закрытие сервиса.
+				bridge.send("VKWebAppClose", {"status": "success"}); // Отправляем bridge на закрытие сервиса.
 			} else {
 				if (history.length > 1) { // Если в массиве больше одного значения:
 					history.pop() // удаляем последний элемент в массиве.
