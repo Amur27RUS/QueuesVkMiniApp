90ecb77e8ccc90ac0bc44c447fd0bf2009623f1a
Andrey Klyuev <Klyuev_Andrey@mail.ru>
2020-10-04 13:13:14 +0300

    fixed some bugs...

--- client/src/panels/CreateQueue.js
+++ client/src/panels/CreateQueue.js
@@ -271,14 +271,14 @@ const CreateQueue = ({ snackbar, id, go, history, setActiveModal, fetchedUser, s
                 </FormLayoutGroup>
 
                 <FormLayoutGroup top="Время проведения">
-                    <Button className={timeInputButton} before={<Icon28RecentOutline/>} stretched={true} size={'xl'} mode={'secondary'} onClick={(qualifiedName, value)=>{
+                    <Button className={timeInputButton} before={<Icon28RecentOutline/>} stretched={true} size={'xl'} mode={'secondary'} onClick={async (qualifiedName, value)=>{
                         document.getElementById('qName').blur();
                         document.getElementById('qDesc').blur();
                         document.getElementById('qPlace').blur();
                         setTimeInput('timeInput');
                         setTimeInputButton('turnOff');
                         setDateInput('turnOff');
-                        setDateInputButton('dateAndTimeInputButton');
+                        await setDateInputButton('dateAndTimeInputButton');
                         document.getElementById('timeID').focus();
 
                     }}>Выбрать время</Button>
