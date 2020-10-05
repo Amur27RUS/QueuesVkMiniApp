cc455f02389de5cb6df051930caebf936865ea80
pavel <pavels21@bk.ru>
2020-10-05 14:17:30 +0300

    add swipe

--- client/src/App.js
+++ client/src/App.js
@@ -304,7 +304,7 @@ const App = (tutorial) =>{
 
 		});
 
-		window.addEventListener('popstate', () => setTimeout(goBack, 1000));
+		window.addEventListener('popstate', () => setTimeout(goBack, 2000));
 
 		async function queuesSet(queuesArray){
 			setQueues(queuesArray);
