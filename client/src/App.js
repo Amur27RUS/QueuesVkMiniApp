0e1b39571606a40f59b4267c158bd5753c935266
pavel <pavels21@bk.ru>
2020-11-22 14:23:05 +0300

    bug fix

--- client/src/App.js
+++ client/src/App.js
@@ -304,7 +304,7 @@ const App = (tutorial) =>{
 
 		});
 
-		window.addEventListener('popstate', () => goBack());
+		window.addEventListener('popstate', () => setTimeout(() => goBack(), 1));
 
 		async function queuesSet(queuesArray){
 			setQueues(queuesArray);
